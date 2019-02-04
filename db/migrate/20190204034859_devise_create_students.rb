fe63e734cfbd7cabff89b8af342caa4653b270af
linhpha <linh-t-pham@uiowa.edu>
2019-02-04 11:43:41 -0600

    PR review

--- db/migrate/20190204034859_devise_create_students.rb
+++ db/migrate/20190204034859_devise_create_students.rb
@@ -20,15 +20,7 @@ class DeviseCreateStudents < ActiveRecord::Migration
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
-
-
-      t.string 'user_id'
-      t.string 'password_digest'
-      t.string 'first_name'
-      t.string 'last_name'
-      t.string 'email_id'
       t.string 'session_token'
-
       t.timestamps
     end
 
