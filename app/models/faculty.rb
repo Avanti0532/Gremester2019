6d25ef94d5ddf7b6bedc3a5e9e5744b446a310da
hpitawela <hpitawela@uiowa.edu>
2019-02-07 21:00:33 -0600

    commit after executing rails generate devise Faculty

---
+++ app/models/faculty.rb
@@ -0,0 +1,8 @@
+class Faculty < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, :email, :username, :password, :uid, :id_card, dpresence: true
+end
