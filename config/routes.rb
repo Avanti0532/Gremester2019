034a154b50b4c424fb0abc392304e327eb17185c
linhpha <linh-t-pham@uiowa.edu>
2019-02-10 14:25:30 -0600

    Looking into create custom login UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :faculties
   devise_for :students
   root to: 'homepage#index'
-
+  match '/log_in', to: 'login#index', via: :get
 end
 # The priority is based upon order of creation: first created -> highest priority.
 # See how all your routes lay out with "rake routes".
