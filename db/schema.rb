d23842dc6d83b562b11f1b555a58f49855de7200
linhpha <linh-t-pham@uiowa.edu>
2019-02-12 12:23:32 -0600

    Fix issue with weblink

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20190209213824) do
 
-ActiveRecord::Schema.define(version: 20190209205855) do
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -34,17 +34,17 @@ ActiveRecord::Schema.define(version: 20190209205855) do
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "faculties", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "username",                            null: false
-    t.string   "first_name",                          null: false
-    t.string   "last_name",                           null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "username",                               null: false
+    t.string   "first_name",                             null: false
+    t.string   "last_name",                              null: false
     t.text     "id_card_data"
-    t.string   "weblink",                             null: false
+    t.string   "weblink",                                null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20190209205855) do
     t.string   "session_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "approved"
+    t.boolean  "approved",               default: false, null: false
   end
 
   add_index "faculties", ["approved"], name: "index_faculties_on_approved"
