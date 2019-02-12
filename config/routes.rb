35c8a6ffb1da3a7c19cd7f73404bd943e54fcc3f
hpitawela <hpitawela@uiowa.edu>
2019-02-11 21:08:32 -0600

    angular-devise removed

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 # require "shrine/storage/file_system"
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  mount ImageUploader.upload_endpoint(:cache) => "/upload"
-  # mount ImageUploader.upload_endpoint(:store) => "public/uploads"
+  # mount ImageUploader.upload_endpoint(:cache) => "upload"
+  mount ImageUploader.upload_endpoint(:store) => "public/uploads"
   devise_for :admins
   devise_for :faculties, controllers: {
         sessions: 'sessions'
