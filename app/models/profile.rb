e853adeaf45cfc1f3e7480e8993ab59dbe3e8ae7
linhpha <linh-t-pham@uiowa.edu>
2019-02-20 13:51:16 -0600

    Add model methods to update field in profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,50 @@
 class Profile < ActiveRecord::Base
   belongs_to :student
   has_many :applications
+  validates_inclusion_of :gre, :in => 0..170
+  validates_inclusion_of :toefl, :in => 0..120
+  validates_numericality_of :cgpa, :greater_than_or_equal_to => 0
+  validates_numericality_of :year_work_exp, :greater_than_or_equal_to => 0
+  validates_numericality_of :month_work_exp, :greater_than_or_equal_to => 0
+  validates_presence_of :student_id
+  def update_gre(gre)
+    self.gre = gre
+  end
+
+  def update_toefl(toefl)
+    self.toefl = toefl
+  end
+
+  def update_cgpa(cgpa)
+    self.cgpa = cgpa
+  end
+
+  def update_interested_major(interested_major)
+    self.interested_major = interested_major
+  end
+
+
+  def update_interested_term(interested_term)
+    self.interested_term = interested_term
+  end
+
+  def update_year_work_experience(year_work_exp)
+    self.year_work_exp = year_work_exp
+  end
+
+  def update_month_work_experience(month_work_exp)
+    self.month_work_exp = month_work_exp
+  end
+
+  def update_resume_data(resume_data)
+    self.resume_data = resume_data
+  end
+
+  def update_sop_data(sop_data)
+    self.sop_data = sop_data
+  end
+
+  def update_additional_attachment_data(additional_attachment_data)
+    self.additional_attachment_data = additional_attachment_data
+  end
 end
