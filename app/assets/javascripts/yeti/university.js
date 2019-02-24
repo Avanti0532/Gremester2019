71010aa0569872bbe1e2710b88c1b4ad3f8fa388
hpitawela <hpitawela@uiowa.edu>
2019-02-24 13:02:44 -0600

    Removed unnecessary comments

--- app/assets/javascripts/yeti/university.js
+++ app/assets/javascripts/yeti/university.js
@@ -1,16 +1,10 @@
 $(document).ready(function () {
-    console.log('In datatable Out');
     $("#universitiesTable").each(function () {
-        console.log('In datatable');
         if ( $.fn.dataTable.isDataTable( '#universitiesTable' ) ) {
             table = $('#universitiesTable').DataTable();
         }
         else {
             table = $('#universitiesTable').DataTable( {
-                // processing: true;
-                // serverSide: true;
-                // ajax: 'universities.json';
-
             });
         }
     });
