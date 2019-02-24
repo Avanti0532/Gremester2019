ec39d6363176039cb25dc1d468f26d420552aaae
hpitawela <hpitawela@uiowa.edu>
2019-02-24 12:31:00 -0600

    Added JS support for DataTables

--- app/assets/javascripts/yeti/university.js
+++ app/assets/javascripts/yeti/university.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
+    console.log('In datatable Out');
     $("#universitiesTable").each(function () {
+        console.log('In datatable');
         if ( $.fn.dataTable.isDataTable( '#universitiesTable' ) ) {
             table = $('#universitiesTable').DataTable();
         }
         else {
-            table = $('#universitiesTable').DataTable(
-        );
+            table = $('#universitiesTable').DataTable( {
+                // processing: true;
+                // serverSide: true;
+                // ajax: 'universities.json';
+
+            });
         }
     });
 })
