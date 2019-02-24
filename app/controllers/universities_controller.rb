d607a5f06906fb85930ffdac74ecc9455a1c9cd5
hpitawela <hpitawela@uiowa.edu>
2019-02-24 13:04:32 -0600

    Added ranking_from, ranking_to fields to controller to set them in select boxes in index view

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -7,10 +7,16 @@ class UniversitiesController < ApplicationController
   def index
     if params[:ranking_from] && params[:ranking_to] && params[:type] then
       @universities = University.where("rank >= ? AND rank <= ? AND university_type = ?", params[:ranking_from], params[:ranking_to], params[:type])
+      @ranking_from = params[:ranking_from]
+      @ranking_to = params[:ranking_to]
+      @type = params[:type]
     elsif params[:ranking_from] && params[:ranking_to] then
       @universities = University.where("rank >= ? AND rank <= ?", params[:ranking_from], params[:ranking_to])
+      @ranking_from = params[:ranking_from]
+      @ranking_to = params[:ranking_to]
     elsif params[:type] then
       @universities = University.where("university_type = ?", params[:type])
+      @type = params[:type]
     else
       @universities = University.all
     end
