008e82509e71e6998fd0fb39131d62ca48a488da
hpitawela <hpitawela@uiowa.edu>
2019-02-24 12:09:06 -0600

    University Search and Filter works, RSpec done

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -4,4 +4,15 @@ class UniversitiesController < ApplicationController
     id = params[:id]
     @university = University.find(id)
   end
+  def index
+    if params[:ranking_from] && params[:ranking_to] && params[:type] then
+      @universities = University.where("rank >= ? AND rank <= ? AND university_type = ?", params[:ranking_from], params[:ranking_to], params[:type])
+    elsif params[:ranking_from] && params[:ranking_to] then
+      @universities = University.where("rank >= ? AND rank <= ?", params[:ranking_from], params[:ranking_to])
+    elsif params[:type] then
+      @universities = University.where("university_type = ?", params[:type])
+    else
+      @universities = University.all
+    end
+  end
 end
--- spec/controllers/universities_controller_spec.rb
+++ spec/controllers/universities_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'rails_helper'
+require 'factory_girl'
 
 describe UniversitiesController do
   describe 'show universities' do
@@ -10,4 +11,53 @@ describe UniversitiesController do
       response.should render_template('show')
     end
   end
+  describe 'GET #index universities' do
+    it 'should populate an array of universities' do
+      university1 = FactoryGirl.create(:university, :university_type => 'Private')
+      university2 = FactoryGirl.create(:university, :university_type => 'Public')
+      get :index
+      expect(assigns(:universities)).to eq([university1,university2])
+    end
+    it 'should render the :index view' do
+      get :index
+      response.should render_template('index')
+    end
+    it 'should populate only public universities when Public type is selected' do
+      university1 = FactoryGirl.create(:university, :university_type => 'Private')
+      university2 = FactoryGirl.create(:university, :university_type => 'Public')
+      university3 = FactoryGirl.create(:university, :university_type => 'Private')
+      university4 = FactoryGirl.create(:university, :university_type => 'Public')
+      params = {:type=>"Public"}
+      get :index, params
+      expect(assigns(:universities)).to eq([university2,university4])
+    end
+    it 'should populate only private universities when Private type is selected' do
+      university1 = FactoryGirl.create(:university, :university_type => 'Private')
+      university2 = FactoryGirl.create(:university, :university_type => 'Public')
+      university3 = FactoryGirl.create(:university, :university_type => 'Private')
+      university4 = FactoryGirl.create(:university, :university_type => 'Public')
+      params = {:type=>"Private"}
+      get :index, params
+      expect(assigns(:universities)).to eq([university1,university3])
+    end
+    it 'should populate only universities whose ranks are between the params' do
+      university5 = FactoryGirl.create(:university, :rank => 5)
+      university15 = FactoryGirl.create(:university, :rank => 15)
+      university45 = FactoryGirl.create(:university, :rank => 45)
+      university65 = FactoryGirl.create(:university, :rank => 65)
+      params = {:ranking_from => 1, :ranking_to => 25}
+      get :index, params
+      expect(assigns(:universities)).to eq([university5,university15])
+    end
+    it 'should populate only universities whose ranks are between the params and selected type' do
+      university5 = FactoryGirl.create(:university, :rank => 5, :university_type => 'Private')
+      university15 = FactoryGirl.create(:university, :rank => 15, :university_type => 'Public')
+      university45 = FactoryGirl.create(:university, :rank => 45, :university_type => 'Private')
+      university65 = FactoryGirl.create(:university, :rank => 65, :university_type => 'Public')
+      params = {:ranking_from => 1, :ranking_to => 50, :type => 'Private'}
+      get :index, params
+      expect(assigns(:universities)).to eq([university5,university45])
+    end
+  end
+
 end
