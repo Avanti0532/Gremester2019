0fe850affe77e2d709a423d993f013b9b02083be
hpitawela <hpitawela@uiowa.edu>
2019-02-27 12:50:28 -0600

    Bug 117 fixed. Added All option to type select field

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -6,16 +6,13 @@ class UniversitiesController < ApplicationController
   end
   def index
     if params[:ranking_from] && params[:ranking_to] && params[:type] then
-      @universities = University.where("rank >= ? AND rank <= ? AND university_type = ?", params[:ranking_from], params[:ranking_to], params[:type])
+      if params[:type] == 'All' then
+        @universities = University.where("rank >= ? AND rank <= ?", params[:ranking_from], params[:ranking_to])
+      else
+        @universities = University.where("rank >= ? AND rank <= ? AND university_type = ?", params[:ranking_from], params[:ranking_to], params[:type])
+      end
       @ranking_from = params[:ranking_from]
       @ranking_to = params[:ranking_to]
-      @type = params[:type]
-    elsif params[:ranking_from] && params[:ranking_to] then
-      @universities = University.where("rank >= ? AND rank <= ?", params[:ranking_from], params[:ranking_to])
-      @ranking_from = params[:ranking_from]
-      @ranking_to = params[:ranking_to]
-    elsif params[:type] then
-      @universities = University.where("university_type = ?", params[:type])
       @type = params[:type]
     else
       @universities = University.all
