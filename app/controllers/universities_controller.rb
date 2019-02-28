cf6306dc4e1c53a678d1aef790837c245a27bd03
hpitawela <hpitawela@uiowa.edu>
2019-02-27 19:21:44 -0600

    Fix for Travis Failure. Reset to old index with extra functionality for All option

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -5,24 +5,25 @@ class UniversitiesController < ApplicationController
     @university = University.find(id)
   end
   def index
-    if params[:ranking_from] || params[:ranking_to] || params[:type] then
-      if params[:ranking_from] && params[:ranking_to] && params[:type] then
-        if params[:type] == 'All' then
-          @universities = University.where("rank >= ? AND rank <= ?", params[:ranking_from],params[:ranking_to])
-        else
-          @universities = University.where("university_type = ? AND rank >= ? AND rank <= ?", params[:type].to_s, params[:ranking_from],params[:ranking_to])
-        end
-      elsif params[:ranking_from] && params[:ranking_to] then
+    if params[:ranking_from] && params[:ranking_to] && params[:type] then
+      if params[:type] == 'All' then
         @universities = University.where("rank >= ? AND rank <= ?", params[:ranking_from],params[:ranking_to])
       else
-        if params[:type] == 'All' then
-          @universities = University.all
-        else
-          @universities = University.where("university_type = ?", params[:type].to_s)
-        end
+        @universities = University.where("university_type = ? AND rank >= ? AND rank <= ?", params[:type], params[:ranking_from],params[:ranking_to])
       end
       @ranking_from = params[:ranking_from]
       @ranking_to = params[:ranking_to]
+      @type = params[:type]
+    elsif params[:ranking_from] && params[:ranking_to] then
+      @universities = University.where("rank >= ? AND rank <= ?", params[:ranking_from], params[:ranking_to])
+      @ranking_from = params[:ranking_from]
+      @ranking_to = params[:ranking_to]
+    elsif params[:type] then
+      if params[:type] == 'All' then
+        @universities = University.all
+      else
+        @universities = University.where("university_type = ?", params[:type])
+      end
       @type = params[:type]
     else
       @universities = University.all
