591ba626779097fba9beae08ee319d88210f8398
hpitawela <hpitawela@uiowa.edu>
2019-03-10 16:46:04 -0500

    Removed unnecessary commments from students/sessions_controller

--- app/controllers/students/sessions_controller.rb
+++ app/controllers/students/sessions_controller.rb
@@ -2,10 +2,4 @@ class Students::SessionsController < Devise::SessionsController
   include Accessible
   skip_before_action :check_user, only: :destroy
 
-  # to override the SessionsController#create and save profile_id to session variable : method 2
-  # def create
-  #   profile = Profile.joins("INNER JOIN students ON students.id=profiles.student_id AND students.username='#{current_student.username}'")
-  #   session[:current_profile] = profile.id
-  #   root_path
-  # end
 end
