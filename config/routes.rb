dceed919c9ff990450dbc96547bf38160c2cc31e
Linh Pham <linh-t-pham@uiowa.edu>
2019-03-10 11:02:49 -0500

    Fixed redirect route to show school page when click on view profile

--- config/routes.rb
+++ config/routes.rb
@@ -31,8 +31,7 @@ Rails.application.routes.draw do
        collection do
          match '/addschools', to: 'profiles#addschools',via: :post
          post  ":id"  => "profiles#update",  :as => 'update'
-         # match '/showSchools', to: 'profiles#showschools',via: :get, :as => 'show'
-         get   ":id"  => "profiles#showschools", :as => 'show'
+         match '/showSchools/:id', to: 'profiles#showschools',via: :get, :as => 'show'
        end
    end
 
