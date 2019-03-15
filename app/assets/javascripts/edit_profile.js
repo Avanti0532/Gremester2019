0257475deb9672915d3188a4c379531bad73a59f
Linh Pham <linh-t-pham@uiowa.edu>
2019-03-15 16:49:56 -0500

    Implement method to add new school to profile

--- app/assets/javascripts/edit_profile.js
+++ app/assets/javascripts/edit_profile.js
@@ -36,9 +36,30 @@ $(document).ready(function () {
                     success: function(jsonData) {
                         data = JSON.parse(jsonData);
                         countries = data.countries;
+                        console.log(countries);
+                        $("#undergrad_country").append('<option selected disabled>Select Country</option>');
                         for(var i=0; i< countries.length; i++){
-                            $("#undergrad_country").append('<option value="' + countries[i] + '">' + countries[i] + '</option>');
+                            $("#undergrad_country").append('<option value="' + countries[i][1] + '">' + countries[i][0] + '</option>');
                         }
+                    },
+
+                    beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+                });
+
+                $.ajax({
+                    url: "/rank_types",
+                    type: 'GET',
+                    contentType: "application/json",
+                    dataType: 'html',
+                    success: function(jsonData) {
+                        data = JSON.parse(jsonData);
+                        ranks = data.rank_types;
+                        console.log(ranks);
+                        $("#undergrad_rank_type").append('<option selected disabled>Select Ranking Type</option>');
+                        for(var i=0; i< ranks.length; i++){
+                            $("#undergrad_rank_type").append('<option value="' + ranks[i][1] + '">' + ranks[i][0] + '</option>');
+                        }
+                        $("#undergrad_rank_type").append('<option>Ranking type not specified</option>');
 
                     },
 
@@ -49,9 +70,33 @@ $(document).ready(function () {
                     {backdrop: true}
                 );
             });
-
         }
+    });
 
+    $('#saveUndergradInfo').click(function(){
+        var school_name = $('#undergrad_school_name').val();
+        var country = $('#undergrad_country').val();
+        console.log('rere');
+        console.log(country);
+        var acceptance_rate =  $('#undergrad_acceptance rate').val();
+        var location =  $('#undergrad_location_text').val();
+        var description =  $('#undergrad_description_text').val();
+        var ranking =  $('#undergrad_ranking_text').val();
+        var rank_type =  $('#undergrad_rank_type').val();
+        var website = $('#undergrad_website_text').val();
+        $.ajax({
+            url: "/undergrad_universities",
+            type: 'POST',
+            data: {university_name: school_name, country: country, acceptance_rate: acceptance_rate, location: location, university_desc: description, ranking: ranking, rank_type: rank_type, university_link: website },
+            dataType: 'html',
+            success: function(data) {
+                console.log(data);
+                $("#addUndergradModal").modal('hide');
+
+            },
+
+            beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+        });
     });
 
 
---
+++ app/controllers/undergrad_universities_controller.rb
@@ -0,0 +1,24 @@
+class UndergradUniversitiesController < ApplicationController
+  def create
+    if (!params[:university_name].blank? and !params[:country].blank?)
+      new_rank = nil
+      if (!params[:ranking].blank? and !params[:rank_type].blank?)
+        rank_type = RankType.find_by_id(params[:rank_type])
+        new_rank = Ranking.new(:rank => params[:ranking])
+        rank_type.rankings << new_rank
+      end
+      new_undergrad_university = UndergradUniversity.new()
+      new_undergrad_university.university_desc = params[:university_desc]
+      new_undergrad_university.acceptance_rate = params[:acceptance_rate]
+      new_undergrad_university.location = params[:location]
+      new_undergrad_university.university_link = params[:university_link]
+      new_undergrad_university.university_name = params[:university_name]
+      new_undergrad_university.country_id = params[:country]
+      if !new_rank.nil?
+        new_undergrad_university.rankings << new_rank
+      end
+      current_student.current_profile.undergrad_universities << new_undergrad_university
+    end
+      redirect_to edit_profile_path(current_student.id)
+  end
+end
