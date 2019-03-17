40388a02a492defa78fe2977c244ae0af4f83649
hpitawela <hpitawela@uiowa.edu>
2019-03-17 07:52:38 -0500

    Removed unnecessary protected word from bottom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :id_card, :weblink, :university_id) }
   end
-  protected
+
 
 end
 
