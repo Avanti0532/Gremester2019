7906f2534501d132f55231f01810a6b5cded94d9
Linh Pham <linh-t-pham@uiowa.edu>
2019-04-06 13:37:39 -0500

    Changed commented for google maps api

--- app/assets/javascripts/undergrad_universities.js
+++ app/assets/javascripts/undergrad_universities.js
@@ -1,13 +1,13 @@
 var map;
 
 function initialize() {
-    // Create a map centered in Pyrmont, Sydney (Australia).
+    // Create a map centered in United States
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 36.2153013, lng: -113.6920816},
         zoom: 5
     });
 
-    // Search for Google's office in Australia.
+    // Search for university's location
     var request = {
         location: map.getCenter(),
         radius: '500',
