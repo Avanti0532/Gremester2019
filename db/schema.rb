8064966ce9f52a023bf8d1e7a4672d44da9cabb4
Linh Pham <linh-t-pham@uiowa.edu>
2019-04-07 10:32:51 -0500

    separate migration for delete profile id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190401015502) do
+ActiveRecord::Schema.define(version: 20190407152854) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
