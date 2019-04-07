44a13d29d16ac9ba76d4c5938c538b31d2c54ef7
Linh Pham <linh-t-pham@uiowa.edu>
2019-04-06 21:22:28 -0500

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 20190401015502) do
 
   create_table "faculty_evaluations", force: :cascade do |t|
     t.integer  "faculty_id"
-    t.integer  "profile_id"
     t.integer  "application_id"
     t.integer  "score"
     t.integer  "ee_background"
@@ -106,7 +105,6 @@ ActiveRecord::Schema.define(version: 20190401015502) do
 
   add_index "faculty_evaluations", ["application_id"], name: "index_faculty_evaluations_on_application_id"
   add_index "faculty_evaluations", ["faculty_id"], name: "index_faculty_evaluations_on_faculty_id"
-  add_index "faculty_evaluations", ["profile_id"], name: "index_faculty_evaluations_on_profile_id"
 
   create_table "grading_scale_types", force: :cascade do |t|
     t.string   "grading_scale_name"
