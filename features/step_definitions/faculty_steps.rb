2f2a4fb417351970cd12de39d27823b23d595760
Avanti0532 <avantiharish-deshmukh@uiowa.edu>
2019-04-14 17:52:20 -0500

    added bdd for evaluation submission for faculty members

--- features/step_definitions/faculty_steps.rb
+++ features/step_definitions/faculty_steps.rb
@@ -230,7 +230,7 @@ end
 When /^I click on (.*?) button$/ do |btn|
   if btn == 'x'
     find('#x_btn').click
-  elsif btn == 'Back'
+  elsif btn == 'Back' || 'Add Evaluation' || 'Back to profile'
     click_on btn
   else
     click_button btn
