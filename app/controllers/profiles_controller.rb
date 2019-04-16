653262423bf1124da17e8255a291006a76571261
juliachaloupka <julia-chaloupka@uiowa.edu>
2019-04-16 18:21:24 -0500

    added school acceptance prediction UI to student profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -410,4 +410,8 @@ class ProfilesController < ApplicationController
       @all_undergrads << {:details => details.gsub(/\n/, '<br/>').html_safe, :id => university.id, :university_name => university_name}
     end
   end
+
+  def sAdmissionChance
+    gon.universities = University.select('id, university_name').order("university_name")
+  end
 end
