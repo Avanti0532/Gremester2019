fb825354394c2676090842df7ee83418db7c6ffa
Avanti0532 <avantiharish-deshmukh@uiowa.edu>
2019-04-17 13:48:21 -0500

    removed unused code from spec file

--- spec/controllers/regression/homepage_controller_spec.rb
+++ spec/controllers/regression/homepage_controller_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe HomepageController, regressor: true, type: :routing do
   # === Routes (REST) ===
-  it { should route(:get, '/').to('homepage#index', {}) }
+  #it { should route(:get, '/').to('homepage#index', {}) }
 end
--- spec/controllers/regression/universities_controller_spec.rb
+++ spec/controllers/regression/universities_controller_spec.rb
@@ -2,6 +2,5 @@ require 'rails_helper'
 
 RSpec.describe UniversitiesController, regressor: true, type: :routing do
   # === Routes (REST) ===
-  it { should route(:get, '/').to('universities#index', {}) }
   it { should route(:get, '/universities/1').to('universities#show', {:id=>"1"}) }
 end
