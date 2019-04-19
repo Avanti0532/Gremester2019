e1e070df1ec37c2cf3e6ddbdd1510b645f62e009
Avanti0532 <avantiharish-deshmukh@uiowa.edu>
2019-04-19 12:13:03 -0500

    deleted homepage controller spec

--- spec/controllers/regression/homepage_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe HomepageController, regressor: true, type: :routing do
-  # === Routes (REST) ===
-  #it { should route(:get, '/').to('homepage#index', {}) }
-end
